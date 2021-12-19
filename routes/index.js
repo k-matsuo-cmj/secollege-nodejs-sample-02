7fa3516fa3e7b0b00ab7a63caf1fe4060cc164a7
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-12-19 15:20:42 +0900

    add /prisma

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,17 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Express' });
+});
+
+// prisma
+const { PrismaClient } = require('@prisma/client');
+
+router.get('/prisma', async (req, res) => {
+  const prisma = new PrismaClient();
+  const allNames = await prisma.names.findMany();
+  res.json(allNames);
 });
 
 module.exports = router;
