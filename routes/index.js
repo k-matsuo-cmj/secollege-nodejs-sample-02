dd11689ea48dd92ba29b402dca6dbb48e788eefc
K.Matsuo <k.matsuo@cybermissions.co.jp>
2021-12-19 15:38:49 +0900

    prisma where,update

--- routes/index.js
+++ routes/index.js
@@ -15,4 +15,75 @@ router.get('/prisma', async (req, res) => {
   res.json(allNames);
 });
 
+// 「田」のつく人だけを取得
+router.get('/prisma01', async (req, res) => {
+  const prisma = new PrismaClient();
+  const names = await prisma.names.findMany({
+    where: {
+      name: { contains: "田" }
+    }
+  });
+  res.json(names);
+});
+
+// OR条件 鈴木さんと東京都の人
+router.get('/prisma02', async (req, res) => {
+  const prisma = new PrismaClient();
+  const names = await prisma.names.findMany({
+    where: {
+      OR: [
+        { name: { contains: "鈴木" }},
+        { address: { contains: "東京都" }}
+      ]
+    }
+  });
+  res.json(names);
+});
+
+// AND条件 名前に「田」が入り、東京都の人
+router.get('/prisma03', async (req, res) => {
+  const prisma = new PrismaClient();
+  const names = await prisma.names.findMany({
+    where: {
+      AND: [
+        { name: { contains: "田" }},
+        { address: { contains: "東京都" }}
+      ]
+    }
+  });
+  res.json(names);
+});
+
+// AND条件 (03とおなじ)
+router.get('/prisma04', async (req, res) => {
+  const prisma = new PrismaClient();
+  const names = await prisma.names.findMany({
+    where: {
+      name: { contains: "田" },
+      address: { contains: "東京都" }
+    }
+  });
+  res.json(names);
+});
+
+// 更新処理 id=3を埼玉県に更新する
+router.get('/upd01', async (req, res) => {
+  const prisma = new PrismaClient();
+  const names = await prisma.names.update({
+    where: {id: 3},
+    data: {address: "埼玉県"}
+  });
+  res.json(names);
+});
+
+// 更新処理 id=3を千葉県に戻す
+router.get('/upd02', async (req, res) => {
+  const prisma = new PrismaClient();
+  const names = await prisma.names.update({
+    where: {id: 3},
+    data: {address: "千葉県"}
+  });
+  res.json(names);
+});
+
 module.exports = router;
